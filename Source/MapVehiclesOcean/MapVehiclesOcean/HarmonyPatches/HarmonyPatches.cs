b278f02513f1fa6d876bd16931564479adab1c9b
oelsart <oelsart@gmail.com>
2026-02-11 03:05:42 +0900

    難破船に宝を設定 複数の研究を追加 より良い難破船マップの生成 ZiplineBelt（プレイヤーポーンが使えるところまで）

--- Source/MapVehiclesOcean/MapVehiclesOcean/HarmonyPatches/HarmonyPatches.cs
+++ Source/MapVehiclesOcean/MapVehiclesOcean/HarmonyPatches/HarmonyPatches.cs
@@ -1,6 +1,5 @@
 ﻿using HarmonyLib;
 using RimWorld;
-using UnityEngine;
 using VehicleMapFramework;
 using Verse;
 
--- Source/MapVehiclesOcean/MapVehiclesOcean/Sites/GenStep_Shipwreck.cs
+++ Source/MapVehiclesOcean/MapVehiclesOcean/Sites/GenStep_Shipwreck.cs
@@ -1,26 +1,81 @@
 ﻿using JetBrains.Annotations;
 using RimWorld;
+using RimWorld.Planet;
 using UnityEngine;
 using Verse;
 
 namespace MapVehicles;
 
-public class GenStep_Shipwreck : GenStep
+public class GenStep_Shipwreck : GenStep_Scatterer
 {
     [UsedImplicitly(ImplicitUseKindFlags.Assign)]
     public List<PrefabDef> wrecks;
 
+    [Unsaved]
+    private readonly List<CellRect> usedRects = [];
+
+    [Unsaved]
+    private PrefabDef tmpWreck;
+
+    [Unsaved]
+    private Rot4 tmpRot;
+
     public override int SeedPart => 651968188;
-    
+
+    private static readonly SimpleCurve NumWrecksCurve =
+    [
+        new CurvePoint(0.6f, 1),
+        new CurvePoint(0.8f, 2),
+        new CurvePoint(0.95f, 3),
+        new CurvePoint(1f, 5)
+    ];
+
+    private static readonly SimpleCurve NumCorpsesCurve =
+    [
+        new CurvePoint(0.5f, 0),
+        new CurvePoint(0.75f, 1),
+        new CurvePoint(0.95f, 3),
+        new CurvePoint(1f, 5)
+    ];
+
+    private static readonly IntRange CorpseAgeRangeDays = new (50, 10000);
+
+    protected override int CalculateFinalCount(Map map) => (int)NumWrecksCurve.Evaluate(Rand.Value);
+
     public override void Generate(Map map, GenStepParams parms)
     {
-        var rot = Rot4.Random;
-        var wreck = wrecks.RandomElement();
-        // if (!RCellFinder.TryFindRandomCellNearTheCenterOfTheMapWith(x =>
-        //         PrefabUtility.CanSpawnPrefab(wreck, map, x, rot, false), map, out var center))
-        //     return;
-        var center = map.Center;
-        PrefabUtility.SpawnPrefab(wreck, map, center, rot, onSpawned: thing =>
+        base.Generate(map, parms);
+        usedRects.Clear();
+        tmpWreck = null;
+        tmpRot = default;
+    }
+
+    protected override bool TryFindScatterCell(Map map, out IntVec3 result)
+    {
+        tmpWreck = wrecks.RandomElement();
+        tmpRot = Rot4.Random;
+        return base.TryFindScatterCell(map, out result);
+    }
+
+    protected override bool CanScatterAt(IntVec3 loc, Map map)
+    {
+        var size = tmpRot.IsVertical ? tmpWreck.size : new IntVec2(tmpWreck.size.z, tmpWreck.size.x);
+        var cellRect = CellRect.CenteredOn(loc, size);
+        return usedRects.All(r => !r.Overlaps(cellRect));
+    }
+
+    protected override void ScatterAt(IntVec3 loc, Map map, GenStepParams parms, int _count = 1)
+    {
+        SpawnWrecks(loc, map, parms);
+    }
+
+    private void SpawnWrecks(IntVec3 loc, Map map, GenStepParams parms)
+    {
+        var size = tmpRot.IsVertical ? tmpWreck.size : new IntVec2(tmpWreck.size.z, tmpWreck.size.x);
+        var cellRect = CellRect.CenteredOn(loc, size);
+        usedRects.Add(cellRect.ExpandedBy(1));
+        
+        PrefabUtility.SpawnPrefab(tmpWreck, map, loc, tmpRot, onSpawned: thing =>
         {
             if (thing.def.useHitPoints)
                 thing.HitPoints = Rand.Range(Mathf.CeilToInt(thing.MaxHitPoints * 0.05f), thing.HitPoints);
@@ -31,7 +86,7 @@ public class GenStep_Shipwreck : GenStep
         var sitePart = parms.sitePart;
         if (sitePart?.things == null || sitePart.things.Count == 0) return;
 
-        
+    
         var containers = map.listerBuildings.allBuildingsNonColonist
             .Where(b => b is IThingHolder && b.Faction is null)
             .Cast<IThingHolder>()
@@ -55,8 +110,26 @@ public class GenStep_Shipwreck : GenStep
 
             if (!placed)
             {
-                sitePart.things.TryDrop(t, center, map, ThingPlaceMode.Near, out _);
+                sitePart.things.TryDrop(t, loc, map, ThingPlaceMode.Near, out _);
             }
+        }
+    }
+
+    private void SpawnCorpses(Map map)
+    {
+        for (var i = 0; i < Rand.Range(0, NumCorpsesCurve.Evaluate(Rand.Value)); i++)
+        {
+            var pawn = PawnGenerator.GeneratePawn(PawnKindDefOf.Drifter);
+            if (!CellFinder.TryFindRandomSpawnCellForPawnNear(usedRects.RandomElement().RandomCell, map, out var cell))
+                continue;
+            pawn.health.SetDead();
+            pawn.apparel.DestroyAll();
+            pawn.equipment.DestroyAllEquipment();
+            Find.WorldPawns.PassToWorld(pawn, PawnDiscardDecideMode.Discard);
+            var corpse = pawn.MakeCorpse(null, null);
+            corpse.Age = Mathf.RoundToInt(CorpseAgeRangeDays.RandomInRange * 60000);
+            corpse.GetComp<CompRottable>().RotProgress += corpse.Age;
+            GenSpawn.Spawn(pawn.Corpse, cell, map);
         }
     }
 }
